ef3cfd468b936ce3e950be67bccd873100637067
KishanPanchal25 <k.d.panchalofc@gmail.com>
2023-03-12 21:21:41 +0530

    working qz tray

--- qz_tray/hooks.py
+++ qz_tray/hooks.py
@@ -14,7 +14,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/qz_tray/css/qz_tray.css"
-# app_include_js = "/assets/qz_tray/js/qz_tray.js"
+app_include_js = "/assets/qz_tray/js/qz_tray.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/qz_tray/css/qz_tray.css"
--- qz_tray/public/js/print_qz.js
+++ qz_tray/public/js/print_qz.js
@@ -1,33 +1,41 @@
+
 frappe.ui.form.on('Sales Invoice', {
   refresh: function(frm) {
+    console.log(frm.doc.docstatus)
     if (frm.doc.docstatus == 1 && !frm.doc.__islocal) {
       frm.add_custom_button(__('Print'), function() {
-        frappe.call({
-          method: 'qz_tray.print_doc.print_sales_invoice',
-          args: {
-            docname: frm.docname
-          },
-          callback: function(r) {
-            if (r.message) {
-              console.log(r);
-              // send the PDF to QZ Tray
-              frappe.require('assets/qz_tray/public/js/qz-tray.js', function() {
-                qz.printers.find('PrinterName').then(function(printer) {
-                  var config = qz.configs.create(printer);
-                  var data = [
-                    { type: 'raw', format: 'pdf', data: r.message }
-                  ];
-                  qz.print(config, data).catch(function(e) {
+        frappe.require('/assets/qz_tray/js/qz-tray.js', function() {
+          
+          frappe.call({
+            method: 'qz_tray.print_doc.print_sales_invoice',
+            args: {
+              docname: frm.docname
+            },
+            callback: function(r) {
+              if (r.message) {
+                // connect to QZ Tray
+                qz.websocket.connect().then(function() {
+                  // send the PDF to QZ Tray
+                  qz.printers.find('Microsoft Print to PDF').then(function(printer) {
+                    var config = qz.configs.create(printer);
+                    var data = [
+                      { type: 'raw', format: 'pdf', data: r.message }
+                    ];
+                    qz.print(config, data).catch(function(e) {
+                      console.error(e);
+                    });
+                  }).catch(function(e) {
                     console.error(e);
                   });
                 }).catch(function(e) {
                   console.error(e);
                 });
-              });
+              }
             }
-          }
+          });
         });
       }).addClass('btn-primary');
     }
   }
 });
+
